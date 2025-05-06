eae266604aa86c272784044bed9b19f552fa048b
chamelzer <148949829+chamelzer@users.noreply.github.com>
2025-05-06 16:23:18 +0200

    tilføjet i index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Hjem from '../views/Hjem.vue'
+import Pointshop from '@/views/Pointshop.vue'
+import Profil from '@/views/Profil.vue'
+import Scanner from '@/views/Scanner.vue'
+import Sorteringsguide from '@/views/Sorteringsguide.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HomeView,
+  },
+  {
+    path: '/hjem',
+    name: 'Hjem',
+    component: Hjem,
+  },
+  {
+    path: '/pointshop',
+    name: 'Pointshop',
+    component: Pointshop,
+  },
+  {
+    path: '/profil',
+    name: 'Profil',
+    component: Profil,
+  },
+  {
+    path: '/scanner',
+    name: 'Scanner',
+    component: Scanner,
+  },
+  {
+    path: '/sorteringsguide',
+    name: 'Sorteringsguide',
+    component: Sorteringsguide,
   },
 ]
 
